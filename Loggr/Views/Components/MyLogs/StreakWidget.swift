86c3ad4b26c9b12c4f26c15edd552a286c10478f
CamScoglio <cameron.scoggs@gmail.com>
2025-07-19 14:28:53 -0400

    MVP (no app blocking)

---
+++ Loggr/Views/Components/MyLogs/StreakWidget.swift
@@ -0,0 +1,8 @@
+//
+//  StreakWidget.swift
+//  Loggr
+//
+//  Created by Cam Scoglio on 7/18/25.
+//
+
+import Foundation
---
+++ Loggr/Views/Components/MyLogs/WeeklyWidget.swift
@@ -0,0 +1,8 @@
+//
+//  WeeklyWidget.swift
+//  Loggr
+//
+//  Created by Cam Scoglio on 7/18/25.
+//
+
+import Foundation
